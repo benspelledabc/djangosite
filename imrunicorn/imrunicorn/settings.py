763da7c7e0ef1079a86bfc55011fe1a82c339510
Sven Davison <SvenDavison@Gmail.com>
2020-01-07 19:52:20 -0500

    working estimated dope!

--- imrunicorn/imrunicorn/settings.py
+++ imrunicorn/imrunicorn/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'imrunicorn.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     },
+     # 'default': {
+     #     'ENGINE': 'django.db.backends.sqlite3',
+     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     # },
 
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'OPTIONS': {
-    #        'read_default_file': '/opt/loaddata.cnf',
-    #        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-    #    },
-    # }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/opt/loaddata.cnf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+     }
 
 }
 
