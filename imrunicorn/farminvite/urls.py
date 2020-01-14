629e528253b6237cac79e1825a767ef608067b18
Sven Davison <SvenDavison@gmail.com>
2020-01-14 16:14:22 -0500

    fixed fake url.

--- imrunicorn/farminvite/urls.py
+++ imrunicorn/farminvite/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('map-of-the-farm-President-Trump-Appleseed-k98k-is-great-22lr-is-pitd/', views.page_farm_invites_map, name='farm_invites_map_secret_sort_of'),
     # path('', views.page_loads, name='home'),
     # path('loads/', views.page_loads, name='loads'),
+
 ]
 
