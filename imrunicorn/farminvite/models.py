cc5653dccccd51f3e58bde494a9d4699caecab69
Sven Davison <SvenDavison@gmail.com>
2020-01-14 16:42:00 -0500

    fixed sort order

--- imrunicorn/farminvite/models.py
+++ imrunicorn/farminvite/models.py
@@ -5,11 +5,6 @@ from django.db import models
 
 # Create your models here.
 class InviteListing(models.Model):
-    # Manufacture = models.CharField(max_length=150, default=None, blank=True, null=True)
-    # Name = models.CharField(max_length=150, default=None, blank=True, null=True)
-    # WeightGR = models.DecimalField(max_digits=5, decimal_places=1)
-    # Diameter = models.DecimalField(max_digits=5, decimal_places=3)
-    # Ballistic_Coefficient = models.DecimalField(max_digits=5, decimal_places=4, default=0.24)
     Invite_Date = models.DateField(default=date.today)
     Invite_AM = models.BooleanField(default=True)
     Invite_PM = models.BooleanField(default=False)
@@ -19,9 +14,8 @@ class InviteListing(models.Model):
     Phone_Number = models.CharField(max_length=20, default=None, blank=True, null=True)
     EMail = models.CharField(max_length=150, default=None, blank=True, null=True)
 
-
     def __str__(self):
-        return "%s %s %s" % (self.Invite_Date, self.MDShooters_Name, self.Phone_Number)
+        return "%s %s (Secondary: %s) %s" % (self.Invite_Date, self.MDShooters_Name, self.Invite_Secondary, self.Phone_Number)
 
     class Meta:
-        ordering = ('Invite_Date', 'MDShooters_Name', 'Phone_Number')
+        ordering = ('Invite_Date', 'Invite_Secondary', 'MDShooters_Name', 'Phone_Number')
