919fab67090d87753d228ed0b94779c3b56e0be8
Sven Davison <SvenDavison@Gmail.com>
2020-02-07 12:56:12 -0500

    fixed the pdf link

--- imrunicorn/polls/urls.py
+++ imrunicorn/polls/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
@@ -9,4 +10,7 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+
+    # ex: /polls/django-pdf
+    path('django-pdf/', views.django_pdf, name='django_pdf'),
 ]
