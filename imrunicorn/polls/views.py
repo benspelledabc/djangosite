7a41d51d6e687091dc32d795bccd845a052ebcb0
Sven Davison <SvenDavison@Gmail.com>
2020-02-07 10:07:33 -0500

    adding polls. stopped at section 2.6, page 37.

--- imrunicorn/polls/views.py
+++ imrunicorn/polls/views.py
@@ -1,6 +1,54 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader # for loader.get_template(), replaced with shortcuts.render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Question
+
+
+def index_orig(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index_version_two(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def index_using_loader(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail_try_except(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+# this is the same as a try/except but less wordy
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 
