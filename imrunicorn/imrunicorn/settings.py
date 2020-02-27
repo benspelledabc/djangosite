2fe593ea56123005162e3b6ae3afcd09a6cb9d7c
Sven Davison <svendavison@gmail.com>
2020-02-27 18:00:41 -0500

    Development (#167)
    * Issue 153 rpm calc (#157)
    
    * init branch check in.
    
    * added RPSecond calculation/display.
    
    * Issue 153 rpm calc (#159)
    
    * init branch check in.
    
    * added RPSecond calculation/display.
    
    * added RPSecond calculation/display.
    
    * added RPSecond calculation/display.
    
    * removed login link if not logged in.
    
    * Added ability to cancel invite. (#164)
    
    * Issue 154 invite cancel (#165)
    
    * Added ability to cancel invite.
    
    * Added ability to cancel invite.
    
    * craaaaaaaaaap
    
    * added django_extensions to use show_urls

--- imrunicorn/imrunicorn/settings.py
+++ imrunicorn/imrunicorn/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'rest_framework',
+    'django_extensions',
     'crispy_forms',
     'imrunicorn',
     'loaddata',
