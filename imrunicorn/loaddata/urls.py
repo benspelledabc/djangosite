e4ee31f241720655716c9529a4338adef1dfba20
Sven Davison <svendavison@gmail.com>
2020-03-10 17:28:20 -0400

    Develop (#183)
    * removed farm invite section.
    
    * updated version info.
    
    * Expire polls (#173)
    
    * Showing up to 15 (sorted by expiration date). only started, not expired polls are displayed.
    
    * .
    
    * .
    
    * .
    
    * bottom to zero
    
    * bottom to zero
    
    * shifted RPS position.
    
    * Changed var name and added windage version.
    
    * Conditional display of click correction.
    
    * added firearm detail view.
    
    * added firearm detail link for sheriff load listings.
    
    * made display of clicks optionally displayed if >=0
    
    * Moved foot pound calc and cleaned up views a little.

--- imrunicorn/loaddata/urls.py
+++ imrunicorn/loaddata/urls.py
@@ -9,7 +9,11 @@ from . import views
 app_name = 'load_data'
 urlpatterns = [
     path('', views.page_loads, name='home'),
+
+    path('foot_pound_calculator/', views.page_foot_pound_calc, name='foot_pound_calculator'),
+
     path('loads/', views.page_loads, name='loads'),
+
     path('estimated_dope/<int:load_pk>', views.page_estimated_dope, name='page_estimated_dope'),
     path('estimated_dope/', views.page_estimated_dope, name='page_estimated_dope'),
 
--- imrunicorn/loaddata/views.py
+++ imrunicorn/loaddata/views.py
@@ -14,6 +14,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def page_foot_pound_calc(request):
+    context = {
+        'release': get_version_json(),
+        "title": "Foot Pound Calculator",
+        "blurb": "I moved the calculator to its own page.",
+        "copy_year": datetime.now().year,
+    }
+    return render(request, "loaddata/foot_pound_calc.html", context)
+
+
 # Create your views here.
 def page_loads(request):
     logger.info("This is not getting logged...")
@@ -25,9 +35,8 @@ def page_loads(request):
 
     context = {
         'release': get_version_json(),
-        "title": "Master Po: Load Data",
-        "blurb": "I'll move it to a database setup in a bit.",
-        "table_data": 'This should be from the database... jackle.',
+        "title": "Load Data",
+        "blurb": "Do your own research, don't use this load data. It might be a pipe bomb in your firearm.",
         'all_loads': all_loads,
         "copy_year": datetime.now().year
     }
@@ -35,12 +44,6 @@ def page_loads(request):
 
 
 def page_estimated_dope(request, load_pk='3'):
-    # all_loads = HandLoad.objects.all().order_by('Is_Sheriff_Load', '-prod', '-projectile__Diameter').annotate(
-    #     prod=ExpressionWrapper(F('projectile__WeightGR') * 0.5 / 7000 / 32.127 * F('Velocity') * F('Velocity'),
-    #                            output_field=FloatField()))
-
-    # selected_load = EstimatedDope.objects.get(hand_load=load_pk)
-
     try:
         selected_load = EstimatedDope.objects.get(hand_load=load_pk)
         context = {
@@ -70,8 +73,8 @@ def page_firearm_detail(request, firearm_pk='3'):
         context = {
             'firearm_id': firearm_pk,
             'release': get_version_json(),
-            "title": "Master Po Load Data",
-            "blurb": "I'll move it to a database setup in a bit.",
+            "title": "Firearm Detail",
+            "blurb": "Sometimes knowing your tool is more important than using it quickly.",
             'firearm_details': selected_firearm,
             "copy_year": datetime.now().year,
         }
