d8d623285b8c53ff471de0ff1b8b47211dc90390
Sven Davison <svendavison@gmail.com>
2020-03-10 21:09:42 -0400

    added sticky bit, but its not in use yet. (#189)

--- imrunicorn/announcements/models.py
+++ imrunicorn/announcements/models.py
@@ -9,11 +9,12 @@ class WhatIsNew(models.Model):
     Blurb = models.CharField(max_length=250)
     Body = models.TextField()  # i like big comments...
     Published = models.BooleanField(default=True)
+    Is_Sticky = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s %s" % (self.Date, self.Blurb)
 
     class Meta:
-        ordering = ('Date', 'Blurb')
+        ordering = ('-Date', 'Blurb')
         verbose_name = 'What Is New'
         verbose_name_plural = 'What Is New'
