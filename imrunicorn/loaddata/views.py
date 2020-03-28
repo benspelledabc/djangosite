abef075b99715bbc86848ef55c4b52a037c5c4f5
Sven Davison <svendavison@gmail.com>
2020-03-28 08:29:57 -0400

    Develop (#219)
    * removed farm invite section.
    
    * updated version info.
    
    * Expire polls (#173)
    
    * Showing up to 15 (sorted by expiration date). only started, not expired polls are displayed.
    
    * .
    
    * .
    
    * .
    
    * bottom to zero
    
    * bottom to zero
    
    * shifted RPS position.
    
    * Changed var name and added windage version.
    
    * Conditional display of click correction.
    
    * added firearm detail view.
    
    * added firearm detail link for sheriff load listings.
    
    * made display of clicks optionally displayed if >=0
    
    * Moved foot pound calc and cleaned up views a little.
    
    * added ben_center to clean up the alignments.
    
    * moar cleanup
    
    * ben_center to ben_format_pritty and fixed firearm_detail view.
    
    * put logon form back on.
    
    * moar cleanup
    
    * moar cleanup
    
    * moar cleanup
    
    * cleaned up the dropdown menus
    
    * shifting to more variables.
    
    * added sticky bit, but its not in use yet.
    
    * added is_sticky control.
    
    * added noodely one to news page.
    
    * added is sticky bit to news API.
    
    * removed duplicate link.
    
    * added news link by PK
    
    * added link to news detail
    
    * added 'read more' links
    
    * added elipses
    
    * updated ignore.
    
    * updated ignore.
    
    * updated ignore.
    
    * updated ignore.
    
    * updated ignore.
    
    * updated ignore.
    
    * yay, sort orders!
    
    * seems to be working.. no fancy ui though (#199)
    
    * Groundhog kill log (#200)
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * Groundhog kill log (#201)
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * Groundhog kill log (#202)
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * almost done adding removals...
    
    * Groundhog kill log (#204)
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * almost done adding removals...
    
    * added pages to display logbook.
    
    * Groundhog kill log (#206)
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * seems to be working.. no fancy ui though
    
    * almost done adding removals...
    
    * added pages to display logbook.
    
    * added pages to display logbook.
    
    * Added page for showing ghog hole list.
    
    * Normalized casing of display text.
    
    * Updated to have db driven main page blurb.
    
    * Updated to have db driven main page blurb.
    
    * Removed MainPageBlurbs in favor of PageBlurbOverrides.
    
    * Updated days_since to use input query string.
    
    * Moar Updatez!
    
    * added links for powder and projectiles
    
    * updated release
    
    * updated release
    
    * removed if checks on clicks.

--- imrunicorn/loaddata/views.py
+++ imrunicorn/loaddata/views.py
@@ -69,7 +69,16 @@ def page_estimated_dope(request, load_pk='3'):
     return render(request, "loaddata/estimated_dope.html", context)
 
 
-def page_firearm_detail(request, firearm_pk='3'):
+def page_firearm_detail(request, firearm_pk=None):
+    if firearm_pk is None:
+        context = {
+            'load_id': firearm_pk,
+            'release': get_version_json(),
+            "title": "Can't get detail without an id.",
+            "blurb": get_page_blurb_override('load_data/firearm_detail/NOT-FOUND'),
+            "copy_year": datetime.now().year,
+        }
+        return render(request, "loaddata/firearm_details.html", context)
     try:
         selected_firearm = Firearm.objects.get(pk=firearm_pk)
 
@@ -86,7 +95,7 @@ def page_firearm_detail(request, firearm_pk='3'):
         context = {
             'load_id': firearm_pk,
             'release': get_version_json(),
-            "title": "Master Po Load Data",
+            "title": "OOPS! Firearm not found.",
             "blurb": get_page_blurb_override('load_data/firearm_detail/NOT-FOUND'),
             "copy_year": datetime.now().year,
         }
