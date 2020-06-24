39e71c662339d4b191a74298015d2e8108b8c9a4
Ben SpelledABC <Staff@BenSpelledABC.me>
2020-06-23 23:13:28 -0400

    cleaned up sex display

--- imrunicorn/groundhog_logbook/functions.py
+++ imrunicorn/groundhog_logbook/functions.py
@@ -36,7 +36,8 @@ def groundhog_removal_scoreboard():
 # check into lookup, transform, aggregates and be jolly!
 def groundhogs_by_hour_of_day():
     result = RemovalsByLocation.objects.annotate(
-        hour=TruncHour('removal_time')).values('hour', 'sex')\
-        .annotate(experiments=Count('id'))\
+        # hour=TruncHour('removal_time')).values('hour', 'sex') \
+        hour=TruncHour('removal_time')).values('hour',) \
+        .annotate(kills_per_hour=Count('id'))\
         .order_by('hour')
     return result
