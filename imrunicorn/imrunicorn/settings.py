471360777a7628d1cc7df78ecf3875ed5a2db954
Sven Davison <SvenDavison@Gmail.com>
2020-08-06 19:50:56 -0400

    more cleanup

--- imrunicorn/imrunicorn/settings.py
+++ imrunicorn/imrunicorn/settings.py
@@ -157,7 +157,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'svendavison@gmail.com'
+EMAIL_HOST_USER = 'username@gmail.com'
 # noinspection PyInterpreter
 EMAIL_HOST_PASSWORD = 'redacted'
 # EMAIL_HOST_PASSWORD = env.str('GMAIL_PASS')
