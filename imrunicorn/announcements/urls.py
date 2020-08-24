389dc8b4873b69911e37f2e5639246b9c8f012f4
Sven Davison <SvenDavison@Gmail.com>
2020-08-24 19:36:37 -0400

    final fix, i think.

--- imrunicorn/announcements/urls.py
+++ imrunicorn/announcements/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 
 # similar to object based url building
 router = routers.DefaultRouter()
-router.register('news_api', views.NewsView)
-router.register('main_page_blurbs', views.MainPageBlurbsView)
-router.register('page_blurb_overrides', views.PageBlurbOverridesView)
-# router.register('courses', views.CourseView)
+# these got moved to 'api' app.
+# router.register('news_api', views.NewsView)
+# router.register('main_page_blurbs', views.MainPageBlurbsView)
+# router.register('page_blurb_overrides', views.PageBlurbOverridesView)
 
 # app_name = 'announcements'
 urlpatterns = [
