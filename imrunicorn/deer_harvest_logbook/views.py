3911235a6ebd8606b771a84df580888c8fa0d920
Ben SpelledABC <svendavison@gmail.com>
2020-08-27 19:56:43 -0400

    Deer harvest tracker (#292)
    * inital model for deer_harvest_logbook
    
    * inital model for deer_harvest_logbook
    
    * added menu item placeholder
    
    * fixed blurb override url

--- imrunicorn/deer_harvest_logbook/views.py
+++ imrunicorn/deer_harvest_logbook/views.py
@@ -19,7 +19,7 @@ def page_all_harvests(request):
         "all_news": all_harvests,
         'release': get_version_json(),
         "title": "Deer Harvest Logbook",
-        "blurb": get_page_blurb_override('deer_harvest_logbook/by_shooter/'),
+        "blurb": get_page_blurb_override('deer_harvest_logbook/'),
         # "blurb": "Coming soon...",
     }
     return render(request, "deer_harvest_logbook/all_harvests.html", context)
