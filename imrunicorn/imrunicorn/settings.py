0d738456abf3b938b727d196a965613ce882a447
Sven <svendavison@gmail.com>
2020-09-07 22:13:46 -0400

    more footer formatting (#304)

--- imrunicorn/imrunicorn/settings.py
+++ imrunicorn/imrunicorn/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'hmj=u6w0i830gw=k^l&vc*jsl!mvtx8#r%#con#lvz04aordkg'
 
-DEBUG = False
+DEBUG = True
 IS_PRODUCTION = not DEBUG
 
 ALLOWED_HOSTS = ['benspelledabc.me', 'localhost', '127.0.0.1', '*']
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'groundhog_logbook',
     'admin_toolbox',
     'deer_harvest_logbook',
+    'deer_waitlist',
 ]
 
 MIDDLEWARE = [
