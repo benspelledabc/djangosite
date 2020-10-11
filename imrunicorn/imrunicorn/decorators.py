566364acf56f260d22cd5d4b2ce1b8449aacebed
Ben SpelledABC <admin@benspelledabc.me>
2020-10-11 11:00:20 -0400

    updates for help on #django

--- imrunicorn/imrunicorn/decorators.py
+++ imrunicorn/imrunicorn/decorators.py
@@ -13,27 +13,35 @@ def unauthenticated_user(view_func):
     return wrapper_func
 
 
-def allowed_groups(allowed_group_list=[]):
+def allowed_groups(allowed_group_list=['']):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
             allowed = False
 
+            # debugging. printing list of allowed_group_list
+            print("DEBUGGING: allowed_group_list ", format(allowed_group_list))
+
             if request.user.groups.exists():
                 user_group_list = request.user.groups.all()
+
+                # debugging. printing list of groups user is in
+                print("DEBUGGING: user_group_list ", format(user_group_list))
+
                 print("Group List:", user_group_list)
 
                 for group_name in user_group_list:
-                    print("group_name:", group_name)
+                    # print("User in group_name:", group_name)
                     if group_name in allowed_group_list:
+                        print("Found valid group!")
                         allowed = True
+                    else:
+                        print("Checked: {0} against {1}".format(group_name, allowed_group_list))
 
             if allowed:
                 return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse("You are not authorized to view this page.")
-
-            # print("Working:", allowed_group_list)
+                return HttpResponse("You are not authorized to view this page. Check your groups and try again.")
 
         return wrapper_func
     return decorator
