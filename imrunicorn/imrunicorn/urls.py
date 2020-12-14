21e194244915c0a3124796ac405ef5c5bd06eb75
Ben SpelledABC <admin@benspelledabc.me>
2020-12-13 19:00:24 -0500

    qr about link and image.

--- imrunicorn/imrunicorn/urls.py
+++ imrunicorn/imrunicorn/urls.py
@@ -14,6 +14,8 @@ from . import forms, views
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
+    path('qr/about/', views.page_qr_about, name='qr_about'),
+
     path('greyscale', views.page_greyscale_test, name='greyscale_test'),
 
     path('admin/', admin.site.urls),
