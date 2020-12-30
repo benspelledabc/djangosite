2c94fe02f4c2d26ad2f7fb12869f23888ab05a4d
Ben SpelledABC <admin@benspelledabc.me>
2020-12-30 16:20:50 -0500

    Working 'coverage' for windows. 64%.

---
+++ imrunicorn/win_coverage.bat
@@ -0,0 +1,5 @@
+python -m coverage erase
+python -m coverage run manage.py test
+python -m coverage report
+python -m coverage html
+"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" C:\Users\sveni\PycharmProjects\djangosite\imrunicorn\htmlcov\index.html
