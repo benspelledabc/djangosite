c15054411f2f17a7f07f4c2be5993278ac5cdac6
Ben SpelledABC <admin@benspelledabc.me>
2021-01-09 20:08:31 -0500

    working on profile extension

--- imrunicorn/loaddata/serializer.py
+++ imrunicorn/loaddata/serializer.py
@@ -13,6 +13,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
+        read_only_fields = ('preferred_display_name',)
+        # exclude = ('',)    # user  ?
         fields = ('preferred_display_name',)
 
 
@@ -23,9 +25,14 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class OwnerSerializer(serializers.ModelSerializer):
-    # owner_display_name = UserProfileSerializer()
+    # user_profile = UserProfileSerializer(required=False)
+    # last_name = UserProfileSerializer(required=False)
     # user_profile = UserProfileSerializer(many=False)
     groups = GroupSerializer(many=True)
+    # profile = serializers.PrimaryKeyRelatedField(many=True, queryset=UserProfile.objects.all(), required=False)
+    profile = serializers.PrimaryKeyRelatedField(
+        queryset=UserProfile.objects.all().values('preferred_display_name')
+        , required=False)
 
     class Meta:
         model = User
@@ -33,6 +40,7 @@ class OwnerSerializer(serializers.ModelSerializer):
                   'last_login',
                   'is_superuser',
                   'username',
+                  'profile',
                   # 'user_profile',
                   'first_name',
                   'last_name',
