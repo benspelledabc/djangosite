3d2785e539341844951d6eb397377362dd65639d
Ben SpelledABC <admin@benspelledabc.me>
2021-02-14 11:13:14 -0500

    BO YA!

--- imrunicorn/announcements/admin.py
+++ imrunicorn/announcements/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import WhatIsNew, MainPageBlurbs, PageBlurbOverrides
+from .models import WhatIsNew, MainPageBlurbs, PageBlurbOverrides, PageSecret
 
 # Register your models here.
 admin.site.register(WhatIsNew)
 admin.site.register(MainPageBlurbs)
 admin.site.register(PageBlurbOverrides)
+admin.site.register(PageSecret)
