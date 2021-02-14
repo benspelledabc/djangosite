69975c93d5c56d9bc8613ca0753b46bd4df44b46
Ben SpelledABC <admin@benspelledabc.me>
2021-02-14 06:53:55 -0500

    task list enabled!

---
+++ imrunicorn/activity_log/functions.py
@@ -0,0 +1,41 @@
+import json
+import datetime
+# from datetime import datetime
+from django.conf import settings
+from .models import Activity, ActivityLog, ActivityPhotoValidation
+from django.db.models import Q, Count
+from django.db.models.functions import TruncHour, TruncMonth, TruncYear
+from django.contrib.auth.models import User
+
+
+def activity_scoreboard():
+    result = {
+        "username": "sample",
+        "points": 43
+    }
+
+    return result
+
+
+def activity_list():
+    result = Activity.objects.all().order_by('-transaction_amount')
+
+    return result
+
+
+def activity_tasks_per_user():
+    result = ActivityLog.objects.all()\
+        .order_by('actor', '-activity__transaction_amount', '-date', '-time')
+
+    # for item in result:
+    #     print("{0} - {1} - {2}".format(item.activity.transaction_amount, item.actor.username, item.activity.name))
+    #     print("{0} - {1} -- {2} {3}".format(item.date, item.time, item.actor.username, item.activity.name))
+
+    return result
+
+
+def activity_photo_validation():
+    result = ActivityPhotoValidation.objects.all()\
+        .order_by('-activity_log__date', '-activity_log__time')
+
+    return result
--- imrunicorn/activity_log/urls.py
+++ imrunicorn/activity_log/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'activity_log'
 urlpatterns = [
     path('', views.page_blank, name='blank'),
+    path('task_list', views.page_task_list, name='task_list'),
+    path('tasks_per_user', views.page_tasks_per_user, name='tasks_per_user'),
+    path('scoreboard', views.page_scoreboard, name='scoreboard'),
+    path('photo_validation', views.page_photo_validation, name='photo_validation'),
 
 ]
