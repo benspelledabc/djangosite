c2e9bd7489fcfa56693d554eaab2b1ab964aa15c
ben spelledabc <admin@benspelledabc.me>
2021-02-17 17:09:27 -0500

    working on scoreboard

--- imrunicorn/activity_log/urls.py
+++ imrunicorn/activity_log/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
+# from views import ChartDataScoreByUser
 from . import views
+from .views import ChartDataScoreByUser
+
 
 app_name = 'activity_log'
 urlpatterns = [
     path('', views.page_blank, name='blank'),
     path('task_list', views.page_task_list, name='task_list'),
     path('tasks_per_user', views.page_tasks_per_user, name='tasks_per_user'),
-    path('scoreboard', views.page_scoreboard, name='scoreboard'),
+    path('current_points', views.page_current_points, name='current_points'),
+
     path('photo_validation', views.page_photo_validation, name='photo_validation'),
+
+    path('charts/scoreboard/', views.page_scoreboard_by_user, name='scoreboard_by_user'),
+    path('api/chart/scoreboard/by_user/data/', ChartDataScoreByUser.as_view()),
 
 ]
