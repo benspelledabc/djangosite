0afb6c08f4271c37df6cf4952705e30edbe9f17b
Ben SpelledABC <admin@benspelledabc.me>
2021-02-22 20:44:33 -0500

    updates n stuff

--- imrunicorn/deer_harvest_logbook/models.py
+++ imrunicorn/deer_harvest_logbook/models.py
@@ -8,6 +8,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class HarvestPhoto(models.Model):
+    kill_shot = models.ImageField(upload_to='uploads/deer_shots/', null=True, blank=True)
+    photo_date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return "%s - %s - %s" % (self.id, self.photo_date, self.kill_shot.url)
+
+    class Meta:
+        ordering = ('-photo_date', '-id')
+
+
 class Harvests(models.Model):
     shooter = models.ForeignKey(User, related_name='deer_harvest_logbook_shooter', on_delete=models.CASCADE, null=True)
     harvest_date = models.DateField(default=date.today)
@@ -27,6 +39,8 @@ class Harvests(models.Model):
     kill_shot = models.ImageField(upload_to='uploads/deer_shots/', null=True, blank=True)
     kill_shot_two = models.ImageField(upload_to='uploads/deer_shots/', null=True, blank=True)
     extra_info = models.TextField(blank=True, null=True)  # i like big comments...
+
+    removal_photos = models.ManyToManyField(HarvestPhoto, blank=True)
 
     UNKNOWN = 'UNKNOWN'
     MALE = 'MALE'
