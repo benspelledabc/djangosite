1cf4222fd16af87e01e7c1fc5f6f9b48f0504815
Ben SpelledABC <admin@benspelledabc.me>
2021-02-26 22:09:25 -0500

    1 chart and some menu updates

--- imrunicorn/deer_harvest_logbook/views.py
+++ imrunicorn/deer_harvest_logbook/views.py
@@ -7,8 +7,29 @@ from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 User = get_user_model()
+
+
+# def page_charts(request):
+#     step_hit_count_by_page(request.path)
+#     logs = harvests_by_hour_of_day()
+#     logs_sexy = harvests_by_sex()
+#     logs_by_score = harvests_by_score()
+#     context = {
+#         "restart": get_restart_notice,
+#         "copy_year": datetime.now().year,
+#         "logs": logs,
+#         "logs_sexy": logs_sexy,
+#         "logs_sexy_hour": logs_by_score,
+#         'release': get_version_json(),
+#         "title": "Harvest Charts",
+#         "blurb": get_page_blurb_override('deer_harvest_logbook/charts/'),
+#     }
+#     return render(request, "deer_harvest_logbook/harvest_charts.html", context)
 
 
 def page_charts(request):
@@ -81,3 +102,42 @@ def page_all_harvests_by_shooter_pk(request, shooter_pk=1):
         "blurb": get_page_blurb_override('deer_harvest_logbook/by_shooter/'),
     }
     return render(request, "deer_harvest_logbook/all_harvests.html", context)
+
+
+# ############ CHARTS #############
+class ChartDataByTime(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        by_hour = harvests_by_hour_of_day()
+        labels = []
+        default_items = []
+
+        for item in by_hour:
+            labels.append(item['hour'])
+
+        for item in by_hour:
+            default_items.append(item['kills_per_hour'])
+
+        data = {
+                "labels": labels,
+                "default": default_items,
+        }
+        return Response(data)
+
+
+def page_charts_by_time(request):
+    step_hit_count_by_page(request.path)
+
+    context = {
+        "graph_api_node": '/deer_harvest_logbook/api/chart/by_time/data/',
+        "graph_header": "# of Deer Removals (By Time)",
+        "graph_message": "We're only showing by the removals by the hour.",
+        "restart": get_restart_notice,
+        "copy_year": datetime.now().year,
+        'release': get_version_json(),
+        "title": "Deer Line Charts",
+        "blurb": get_page_blurb_override('deer_harvest_logbook/graphic_charts/'),
+    }
+    return render(request, "deer_harvest_logbook/deer_graphic_generic.html", context)
