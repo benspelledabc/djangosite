1c774a4e06bde7d13286fbe4493e6942c4d1b2e7
ben spelledabc <admin@benspelledabc.me>
2021-03-02 19:16:09 -0500

    moar changes

--- imrunicorn/shooting_challenge/urls.py
+++ imrunicorn/shooting_challenge/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'shooting_challenge'
 urlpatterns = [
     path('', views.page_home, name='home'),
-    path('list', views.page_shooting_challenges_list, name='list'),
+    path('list/', views.page_shooting_challenges_list, name='list'),
+    path('list/<int:challenge_pk>', views.page_shooting_challenges_list_by_pk, name='list'),
 
 ]
