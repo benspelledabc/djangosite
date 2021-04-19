39b2c9b7e46f1b64af9c9be46411750f92bd0653
Ben SpelledABC <admin@benspelledabc.me>
2021-04-19 17:53:18 -0400

    added puppy power

--- imrunicorn/puppy_fostering/views.py
+++ imrunicorn/puppy_fostering/views.py
@@ -15,3 +15,25 @@ def page_home(request):
         "blurb": get_page_blurb_override('puppy_fostering/home/'),
     }
     return render(request, "puppy_fostering/home.html", context)
+
+
+def page_momma_pics(request):
+    step_hit_count_by_page(request.path)
+    context = {
+        "copy_year": datetime.now().year,
+        'release': get_version_json(),
+        "title": "Puppy Fostering: Momma Pics",
+        "blurb": get_page_blurb_override('puppy_fostering/home/'),
+    }
+    return render(request, "puppy_fostering/home.html", context)
+
+
+def page_puppy_pics(request):
+    step_hit_count_by_page(request.path)
+    context = {
+        "copy_year": datetime.now().year,
+        'release': get_version_json(),
+        "title": "Puppy Fostering: Puppy Pics",
+        "blurb": get_page_blurb_override('puppy_fostering/home/'),
+    }
+    return render(request, "puppy_fostering/home.html", context)
