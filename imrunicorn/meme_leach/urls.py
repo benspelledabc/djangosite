2f3efcb75418b461f77a0fd8b10e221b16ea8517
Ben SpelledABC <admin@benspelledabc.me>
2021-04-30 19:23:11 -0400

    bam son

--- imrunicorn/meme_leach/urls.py
+++ imrunicorn/meme_leach/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 
-# router = routers.DefaultRouter()
-# router.register('roll', views.RollView)
-# router.register('student', views.StudentView)
-# router.register('courses', views.CourseView)
 
 app_name = 'meme_leach'
 urlpatterns = [
-    path('', views.sample, name='home'),
+    path('', views.page_sfw_meme_list, name='home'),
+    path('sfw-list/', views.page_sfw_meme_list, name='sfw_meme_list'),
+    path('nsfw-list/', views.page_nsfw_meme_list, name='nsfw_meme_list'),
+    path('fetch/', views.page_meme, name='subreddit'),
+    path('fetch/<str:subreddit>/', views.page_meme_by_subreddit, name='subreddit'),
 ]
 
